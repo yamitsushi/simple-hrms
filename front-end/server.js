18bfeec55e7b0329fa27f33ae584961ad8ccc376
Russel Dave Cruz <russelcruz01@gmail.com>
2021-04-22 00:33:30 +0800

    fix env bug

--- front-end/server.js
+++ front-end/server.js
@@ -10,8 +10,8 @@ app.get("/", function (req, res) {
 });
 
 let listener = app.listen(
-	process.env.EXPRESS_PORT || 3000,
-	process.env.EXPRESS_HOST || "127.0.0.1",
+	process.env.PORT || 3000,
+	process.env.HOST || "127.0.0.1",
 	() => {
 		console.log(
 			`Listening to ${listener.address().address}:${listener.address().port}`
