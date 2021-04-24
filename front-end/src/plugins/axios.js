6fbe09cccd290b48272514f894466d92968386f9
Russel Dave Cruz <russelcruz01@gmail.com>
2021-04-24 23:22:32 +0800

    add loading screen

--- front-end/src/plugins/axios.js
+++ front-end/src/plugins/axios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useState } from "react";
+import { CModal, CModalBody, CProgress } from "@coreui/react";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
@@ -7,5 +9,37 @@ const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
+
+const LoadingModal = () => {
+  const [loading, setLoading] = useState(false);
+
+  axiosInstance.interceptors.request.use((config) => {
+    setLoading(true);
+    return config;
+  });
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      setLoading(false);
+      return response;
+    },
+    (error) => {
+      setLoading(false);
+      // pending
+      // clear user if returned 401 error and have user
+      return Promise.reject(error);
+    }
+  );
+  return (
+    <CModal show={loading} closeOnBackdrop={false} centered={true} size="sm">
+      <CModalBody className="text-center">
+        <h6>Loading</h6>
+        <CProgress animated value={100} />
+      </CModalBody>
+    </CModal>
+  );
+};
+
+export { LoadingModal };
 
 export default axiosInstance;
