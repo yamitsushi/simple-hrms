dcc2bf212a9bee637e93b3a45c608aa8459fb95c
Russel Dave Cruz <russelcruz01@gmail.com>
2021-04-28 23:40:07 +0800

    set universal redirect

--- front-end/server.js
+++ front-end/server.js
@@ -5,16 +5,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 let listener = app.listen(
-	process.env.PORT || 3000,
-	process.env.HOST || "127.0.0.1",
-	() => {
-		console.log(
-			`Listening to ${listener.address().address}:${listener.address().port}`
-		);
-	}
+  process.env.PORT || 3000,
+  process.env.HOST || "127.0.0.1",
+  () => {
+    console.log(
+      `Listening to ${listener.address().address}:${listener.address().port}`
+    );
+  }
 );
