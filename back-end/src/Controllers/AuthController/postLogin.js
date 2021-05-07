75d1bdcc968d50eb5a6f6488ece200345c49b75a
Russel Dave Cruz <russelcruz01@gmail.com>
2021-05-07 15:28:18 +0800

    refactor login

--- back-end/src/Controllers/AuthController/postLogin.js
+++ back-end/src/Controllers/AuthController/postLogin.js
@@ -26,7 +26,6 @@ export default async (req, res) => {
 
 		return res.status(200).json(pluckUserData(user));
 	} catch (err) {
-		console.log(err);
 		return res.status(401).json({
 			title: "Authentication Failed",
 			message: "Incorrect Login Credentials",
