4657bf6238f35f54999bd076dcfc450b990d63ac
Russel Dave Cruz <russelcruz01@gmail.com>
2021-05-07 23:27:34 +0800

    refactor tables

--- front-end/src/views/employees/Table.js
+++ front-end/src/views/employees/Table.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import {
   TableContainer,
   Paper,
@@ -9,79 +8,12 @@ import {
   TableCell,
   TableBody,
   Button,
-  IconButton,
   TableFooter,
   TablePagination,
 } from "@material-ui/core";
-import {
-  FirstPage as FirstPageIcon,
-  LastPage as LastPageIcon,
-  KeyboardArrowRight,
-  KeyboardArrowLeft,
-} from "@material-ui/icons";
 import { CModal } from "@coreui/react";
 import ModifyForm from "./forms/ModifyForm";
-
-const paginateStyle = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
-}));
-
-function TablePaginationActions(props) {
-  const classes = paginateStyle();
-  const { count, page, rowsPerPage, onChangePage } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onChangePage(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onChangePage(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onChangePage(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {<FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {<KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {<KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {<LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
+import TablePaginationActions from "src/components/table/TablePaginationActions";
 
 const Listing = ({ table, departments, positions }) => {
   const [page, setPage] = React.useState(0);
